c5b3c9654531098e774f1bb9f007fd3ca24ba699
Pedro Henrique <pedrohrezendeb07@gmail.com>
2024-09-19 10:21:26 -0300

    criação da classe Aluno

---
+++ cadastro-escola-infantil/src/Aluno.java
@@ -0,0 +1,16 @@
+public class Aluno {
+    private String nome;
+    private String matricula;
+    private String dataNascimento;
+    private String nomeMae;
+
+    public Aluno(String nome, String matricula, String dataNascimento, String nomeMae) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.dataNascimento = dataNascimento;
+        this.nomeMae = nomeMae;
+    }
+    public String toString(){
+        return "Nome: " + nome + "Matrícula: " + matricula + "Data de nascimento: " + dataNascimento + "Nome da mãe: " + nomeMae;
+    }
+}
